0f4c7e5150d3af87e9ba6d83ae97e646e851ba9b
Антон <burlachenko.anton2000@gmail.com>
2025-02-08 12:55:10 +0300

    Initial

---
+++ TestFinancialService/TestFinancialService.API/Controllers/FinancialInstrumentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestFinancialService.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class FinancialInstrumentsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAvailable()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCurrentPrice([Required] string pair)
+        {
+            return Ok();
+        }
+    }
+}
